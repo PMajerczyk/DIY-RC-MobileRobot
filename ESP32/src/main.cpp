8de74c47e444895fe4ce167e292ed753d8db8504
PMajerczyk <pawel.majerczyk@onet.eu>
2023-07-03 23:23:39 +0200

    BT and Servo

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,18 +1,25 @@
-#include <Arduino.h>
+#include "BluetoothSerial.h"
+#include "ESP32Servo.h"
 
-// put function declarations here:
-int myFunction(int, int);
+BluetoothSerial Bluetooth;
+Servo Servo_1, Servo_2;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  Serial.begin(9600);
+  Bluetooth.begin();
+  Servo_1.attach(8);
+  Servo_2.attach(9);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int data_L = 0, data_R = 0;
+  if (Bluetooth.available() >= sizeof(int) * 2) {
+    data_L = Bluetooth.read();
+    data_R = Bluetooth.read();
+  }
+  Servo_1.writeMicroseconds(data_L);
+  Servo_2.writeMicroseconds(data_R);
+  delay(100);
 }
